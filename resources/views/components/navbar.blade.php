cc1b7b781a50165a59244b9238721f6836d3b10d
Doggo090 <hataraku213@gmail.com>
2025-09-08 20:58:09 -0500

    ...

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -22,9 +22,9 @@
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul
                 class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <a href="#home"
-                        class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:bg-blue-700 focus:text-white md:focus:bg-transparent md:focus:text-blue-700 dark:focus:text-white md:dark:focus:text-blue-500">Home</a>
+                <li >
+                    <p href="#home"
+                        class="block py-2 px-3 text-blue-700 rounded-sm hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-blue-700 md:dark:hover:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white md:dark:hover:bg-transparent focus:bg-blue-700 focus:text-white md:focus:bg-transparent md:focus:text-blue-700 dark:focus:text-white md:dark:focus:text-blue-500">Home</p>
                 </li>
                 <li>
                     <a href="#About"
